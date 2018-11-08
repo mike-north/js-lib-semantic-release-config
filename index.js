1514c8ccebff58807e872a4cb993085014a73ff7
Mike North <michael.l.north@gmail.com>
2018-11-08 09:58:17 -0800

    fix: publish docs on release

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
 module.exports = {
-  "verifyConditions": [
-    "@semantic-release/github",
-    "@semantic-release/npm"
-  ],
-  "prepare": [
+  verifyConditions: ['@semantic-release/github', '@semantic-release/npm'],
+  prepare: [
     {
-      "path": "@semantic-release/changelog",
-      "changelogFile": "CHANGELOG.md"
+      path: '@semantic-release/changelog',
+      changelogFile: 'CHANGELOG.md'
     },
     {
-      "path": "@semantic-release/git",
-      "assets": [
-        "package.json",
-        "CHANGELOG.md"
-      ],
-      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      path: '@semantic-release/git',
+      assets: ['package.json', 'CHANGELOG.md', 'docs/**', 'etc/**'],
+      message:
+        'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
     },
-    "@semantic-release/npm"
+    '@semantic-release/npm'
   ],
-  "publish": [
-    "@semantic-release/npm",
-    "@semantic-release/github"
-  ],
-  "success": [
-    "@semantic-release/github"
-  ],
-  "fail": [
-    "@semantic-release/github"
-  ]
-}
+  publish: ['@semantic-release/npm', '@semantic-release/github'],
+  success: ['@semantic-release/github'],
+  fail: ['@semantic-release/github']
+};
